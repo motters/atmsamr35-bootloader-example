bf52c3d1690392dae10ea31f217cdf1abf1bb5e4
Sam Mottley <sammottleys@gmail.com>
2020-07-23 15:42:30 +0100

    Framework library created to house shared files between boot & application

--- framework/flash.h
+++ framework/flash.h
@@ -1,2 +1,10 @@
 #pragma once
 
+#include <stdint.h>
+
+#include <nvm.h>
+
+void config_nvm(void);
+void write_firmware_demo(void);
+void debug_read_app_flash(uint8_t eitherSide, uint8_t *page_buffer);
+void write_page(uint32_t page, uint8_t *change_page_buffer);
